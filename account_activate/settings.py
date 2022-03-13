16b44ce815d8ef1d6478a888d53bb92471859190
olakayCoder1 <programmerolakay@gmail.com>
2022-03-13 03:39:42 -0700

    common sense

--- account_activate/settings.py
+++ account_activate/settings.py
@@ -125,9 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = env("EMAIL_BACKEND")
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'programmerolakay@gmail.com'
-EMAIL_HOST_PASSWORD = 'olakay@pyDev'
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST= env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
